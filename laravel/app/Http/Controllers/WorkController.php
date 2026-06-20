3e3a744e3b49c651ed2053c6a414fa06cd034e94
Fikusnoe <zrocev12@gmail.com>
2026-06-20 16:53:15 +0000

    Создал страницы каталогов + настроил роутинг + настроил контроллер

--- laravel/app/Http/Controllers/WorkController.php
+++ laravel/app/Http/Controllers/WorkController.php
@@ -8,17 +8,13 @@ use Illuminate\View\View;
 
 class WorkController extends Controller
 {
-    /**
-     * Главная страница
-     */
+    /* Главная страница */
     public function index(): View
     {
         return view('home');
     }
 
-    /**
-     * Страница "Фильмы"
-     */
+    /* Страница "Фильмы" */
     public function movies(): View
     {
         $works = Work::where('type', 'movie')->orderBy('release_year', 'desc')->paginate(10);
@@ -30,9 +26,7 @@ class WorkController extends Controller
         ]);
     }
 
-    /**
-     * Страница "Сериалы"
-     */
+    /* Страница "Сериалы" */
     public function series(): View
     {
         $works = Work::where('type', 'series')->orderBy('release_year', 'desc')->paginate(10);
@@ -44,9 +38,7 @@ class WorkController extends Controller
         ]);
     }
 
-    /**
-     * Страница "Игры"
-     */
+    /* Страница "Игры" */
     public function games(): View
     {
         $works = Work::where('type', 'game')->orderBy('release_year', 'desc')->paginate(10);
@@ -58,9 +50,7 @@ class WorkController extends Controller
         ]);
     }
 
-    /**
-     * Страница "Книги"
-     */
+    /* Страница "Книги" */
     public function books(): View
     {
         $works = Work::where('type', 'book')->orderBy('release_year', 'desc')->paginate(10);
@@ -69,6 +59,63 @@ class WorkController extends Controller
             'works' => $works,
             'title' => 'Книги',
             'currentType' => 'book'
+        ]);
+    }
+
+    /* Главная страница каталога */
+    public function catalogIndex()
+    {
+        return view('catalog.catalog');
+    }
+
+    /* Подраздел каталога: Фильмы */
+    public function catalogMovies(Request $request)
+    {
+        return $this->getFilteredCatalog($request, 'movie', 'Каталог фильмов');
+    }
+
+    /* Подраздел каталога: Сериалы */
+    public function catalogSeries(Request $request)
+    {
+        return $this->getFilteredCatalog($request, 'series', 'Каталог сериалов');
+    }
+
+    /* Подраздел каталога: Игры */
+    public function catalogGames(Request $request)
+    {
+        return $this->getFilteredCatalog($request, 'game', 'Каталог игр');
+    }
+
+    /* Подраздел каталога: Книги */
+    public function catalogBooks(Request $request)
+    {
+        return $this->getFilteredCatalog($request, 'book', 'Каталог книг');
+    }
+
+    private function getFilteredCatalog(Request $request, string $type, string $title)
+    {
+        $query = Work::where('type', $type);
+
+        if ($request->filled('search')) {
+            $query->where('title', 'LIKE', '%' . $request->input('search') . '%');
+        }
+
+        if ($request->filled('genre')) {
+            $query->where('genre', $request->input('genre'));
+        }
+
+        $genres = Work::where('type', $type)
+            ->whereNotNull('genre')
+            ->distinct()
+            ->pluck('genre');
+
+        $works = $query->paginate(12);
+
+        return view('catalog.themedCatalog', [
+            'works' => $works,
+            'genres' => $genres,
+            'title' => $title,
+            'currentType' => $type
         ]);
     }
 }
